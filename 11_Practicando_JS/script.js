a4d59c3ed38d599a5f612af81389882ed2a82f27
OsTn0928 <vicenterojas@liceovvh.cl>
2025-03-31 10:35:41 -0300

    ok

---
+++ 11_Practicando_JS/script.js
@@ -0,0 +1,6 @@
+//Tabla de multiplicar personalizadaPide al usuario un número mediante prompt( ) Utiliza un bucle for para imprimir su tabla de multiplicar del 1 al 10. (Ejemplo: si ingresa 3 → imprime 3x1, 3x2... hasta 3x10)
+
+function tablaMultiplicar() {
+    let numero = prompt("Ingrese un número");
+    for (let i = 1; i <= 10; i++) {}
+}
