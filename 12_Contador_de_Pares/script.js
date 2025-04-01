8326ed6b153393e5fcdce58e9d2e4b98cd17b2af
OsTn0928 <vicenterojas@liceovvh.cl>
2025-04-01 11:16:18 -0300

    ok

---
+++ 12_Contador_de_Pares/script.js
@@ -0,0 +1,15 @@
+function funcionPares(){
+    let arrey = arrayNum.split(",");
+    let arrayNum = parseInt(prompt("ingrese numeros separados por una coma: "));
+    let pares = 0;
+
+    arrey.lenght;
+    for(let i = 0; i < arrey.lenght; i++){
+        arrey[i];
+        if (i % 2 === 0){
+            
+        }
+    }
+}
+
+
